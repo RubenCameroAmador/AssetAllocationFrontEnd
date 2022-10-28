0fc684af95f20d03d53dafcc7cb61f6ad0a2ec7e
RubenCameroAmador <37782994+RubenCameroAmador@users.noreply.github.com>
2022-10-28 11:08:01 -0500

    quite la ruta exacta

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export class App extends Component {
       <BrowserRouter>
         <Routes >
           <Route exact path="/" element={<Login/>} />
-          <Route exact path="/game2030/:id" element={<Game/>} />
+          <Route path="/game2030/:id" element={<Game/>} />
         </Routes >
       </BrowserRouter>
     )
